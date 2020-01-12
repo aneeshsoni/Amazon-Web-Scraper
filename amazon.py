02e1912fd761f01d9125c73a8508a2e57824ff34
Aneesh Soni <aneeshsoni@utexas.edu>
2020-01-12 12:57:02 -0600

    test'

--- amazon.py
+++ amazon.py
@@ -49,6 +49,7 @@ def send_email(price, desired_price, product):
 def send_text(message_body):
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
     client = Client(account_sid, auth_token)
 
     message = client.messages \
