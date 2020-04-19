7ddc7692bdecaf9cef314578106292b1417302f2
Aneesh Soni <aneeshsoni@utexas.edu>
2020-04-18 23:06:10 -0400

    updated user agent

--- amazon.py
+++ amazon.py
@@ -11,7 +11,10 @@ from google.auth.transport.requests import Request
 from twilio.rest import Client
 import dateutil
 
-headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Safari/605.1.15'}
+headers = {
+    "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15'
+}
+
 
 class Product:
     def __init__(self):
@@ -26,6 +29,7 @@ class Product:
     def set_name(self, name):
         self.name = name
 
+
 def create_message(sender, to, subject, message_text):
     message = MIMEText(message_text)
     message['to'] = to
@@ -33,24 +37,29 @@ def create_message(sender, to, subject, message_text):
     message['subject'] = subject
     return {'raw': base64.urlsafe_b64encode(message.as_string())}
 
+
 def send_message(service, user_id, message):
     # service: an authorized Gmail API service instance.
     # user_id: User's email address. To indicate the authenticated user, the special value "me" can be used.
     # message: Message to be sent.
 
     try:
-        message = (service.users().messages().send(userId=user_id, body=message).execute())
-        print ('Message Id: %s'), message['id']
+        message = (service.users().messages().send(
+            userId=user_id, body=message).execute())
+        print('Message Id: %s'), message['id']
         return message
     except errors.HttpError as error:
-        print ('An error occurred: %s'), error
+        print('An error occurred: %s'), error
+
 
 def send_email(price, desired_price, product):
     print('Price lower than desired')
 
+
 def set_path():
     env_path = Path('./Twilio.env')
     load_dotenv(dotenv_path=env_path)
+
 
 def send_text(message_body):
     set_path()
@@ -66,6 +75,7 @@ def send_text(message_body):
                         to='+18458006688'
                     )
 
+
 def get_product_info(URL, headers):
     page = requests.get(URL, headers=headers)
     soup = BeautifulSoup(page.content, 'lxml')
@@ -75,20 +85,26 @@ def get_product_info(URL, headers):
 
     return(int(price), name)
 
+
 def set_product_info(price, name, product):
     product.set_name(name)
     product.set_price(price)
 
+
 def compare_price(product):
     if (product.price < product.desired_price):
-        message = 'The price for ' + product.name + ' has dropped below $' + str(product.desired_price) + '!' + '\n\n' + ' Find the link here: ' + product.URL
+        message = 'The price for ' + product.name + ' has dropped below $' + \
+            str(product.desired_price) + '!' + '\n\n' + \
+            ' Find the link here: ' + product.URL
         send_text(message)
+
 
 def main():
     product = Product()
     price, name = get_product_info(product.URL, headers)
     set_product_info(price, name, product)
     compare_price(product)
+
 
 if __name__ == "__main__":
     main()
